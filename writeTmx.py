d360ce176b5f4e429ba52c034902faaa53a485ad
Daniel Schick <daniel.schick6@googlemail.com>
2024-04-14 21:35:07 +0200

    remove weird characters

--- writeTmx.py
+++ writeTmx.py
@@ -1,7 +1,9 @@
 # read sqlite
 # with leftjoin
 import sqlite3
+from re import sub
 from translate.storage.tmx import tmxfile
+
 
 conn = sqlite3.connect('norwegian.sqlite')
 c = conn.cursor()
@@ -23,8 +25,37 @@ TARGET_LANG = "de-DE"
 tmxfile = tmxfile(None, SOURCE_LANG, TARGET_LANG)
 
 for row in all_rows:
-    src_line = row[4].strip()
-    tgt_line = row[9].strip()
+    
+    src_line : str = row[4]
+    tgt_line : str = row[9]
+    
+    #remove weird closing bracket in norwegian 1930
+    tgt_line = tgt_line.replace(":)", ")")
+    src_line = src_line.replace(":)", ")")
+    
+    #remove (-) sign
+    src_line = src_line.replace("(-)", " ")
+    tgt_line = tgt_line.replace("(-)", " ")
+    
+    src_line = src_line.replace(":", ": ")
+    tgt_line = tgt_line.replace(":", ": ")
+    
+    src_line = src_line.replace(",", ", ")
+    tgt_line = tgt_line.replace(",", ", ")
+    
+    src_line = src_line.replace(".", ". ")
+    tgt_line = tgt_line.replace(".", ". ")
+    
+    src_line = src_line.replace(";", "; ")
+    tgt_line = tgt_line.replace(";", "; ")
+    
+    #remove double space - is due to the previous statement, it will produce doulbe spaces in the text
+    src_line = src_line.replace("  ", " ")
+    tgt_line = tgt_line.replace("  ", " ")
+    
+    src_line = src_line.strip()
+    tgt_line = tgt_line.strip()
+    
     tmxfile.addtranslation(src_line, SOURCE_LANG, tgt_line, TARGET_LANG)
 with open("de_bible_prototype.tmx", "wb") as output:
     tmxfile.serialize(output)
